cd4b2dd723df4fc381afd42aead302fdbf84c408
Teye Heimans <teye@level23.nl>
2019-08-14 14:05:21 +0200

    Some more tests

--- tests/QueryBuilderTest.php
+++ tests/QueryBuilderTest.php
@@ -3,14 +3,17 @@ declare(strict_types=1);
 
 namespace tests\Level23\Druid;
 
+use Level23\Druid\Collections\IntervalCollection;
 use Level23\Druid\DruidClient;
+use Level23\Druid\Queries\TimeSeriesQuery;
 use Level23\Druid\QueryBuilder;
+use Mockery;
 use tests\TestCase;
 
 class QueryBuilderTest extends TestCase
 {
     /**
-     * @var \Level23\Druid\DruidClient
+     * @var \Level23\Druid\DruidClient|\Mockery\MockInterface|\Mockery\LegacyMockInterface
      */
     protected $client;
 
@@ -21,7 +24,42 @@ class QueryBuilderTest extends TestCase
 
     public function setUp(): void
     {
-        $this->client  = new DruidClient([]);
-        $this->builder = \Mockery::mock(QueryBuilder::class, [$this->client, 'http://']);
+        $this->client  = Mockery::mock(DruidClient::class, [[]]);
+        $this->builder = Mockery::mock(QueryBuilder::class, [$this->client, 'http://']);
+        $this->builder->makePartial();
+    }
+
+    /**
+     * @throws \Level23\Druid\Exceptions\DruidException
+     * @throws \Level23\Druid\Exceptions\DruidQueryException
+     */
+    public function testExecute()
+    {
+        $context = ['foo' => 'bar'];
+        $query   = Mockery::mock(TimeSeriesQuery::class, ['test', new IntervalCollection()]);
+
+        $result = ['result' => 'here'];
+
+        $normalized = ['normalized' => 'result'];
+
+        $this->builder->shouldAllowMockingProtectedMethods()
+            ->shouldReceive('buildQueryAutomatic')
+            ->with($context)
+            ->once()
+            ->andReturn($query);
+
+        $this->client->shouldReceive('executeDruidQuery')
+            ->once()
+            ->with($query)
+            ->andReturn($result);
+
+        $query->shouldReceive('parseResponse')
+            ->once()
+            ->with($result)
+            ->andReturn($normalized);
+
+        $response = $this->builder->execute($context);
+
+        $this->assertEquals($normalized, $response);
     }
 }
