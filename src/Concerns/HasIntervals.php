89385594bc3bd6c129af1f73b3ca29031821564d
Marc van der Meulen <marc@level23.nl>
2019-08-16 09:16:32 +0200

    Make laravel ready and refactor somethings

--- src/Concerns/HasIntervals.php
+++ src/Concerns/HasIntervals.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 namespace Level23\Druid\Concerns;
 
 use DateTime;
-use Exception;
-use Level23\Druid\Exceptions\DruidException;
 use Level23\Druid\Interval\Interval;
 
 trait HasIntervals
@@ -17,32 +15,21 @@ trait HasIntervals
 
     /**
      * Add an interval, eg the date where we want to select data from.
-     * This can be an Carbon or DateTime object, an unix timestamp or a string which can be parsed to a datetime.
      *
-     * @param \DateTime|string|int $start
-     * @param \DateTime|string|int $stop
+     * @param \DateTime|string|int $start DateTime object, unix timestamp or string accepted by DateTime::__construct
+     * @param \DateTime|string|int $stop  DateTime object, unix timestamp or string accepted by DateTime::__construct
      *
      * @return $this
-     * @throws \Level23\Druid\Exceptions\DruidException
+     * @throws \Exception
      */
     public function interval($start, $stop)
     {
-        try {
-            if (is_numeric($start)) {
-                $start = new DateTime("@$start");
-            } elseif (!$start instanceof DateTime) {
-                $start = new DateTime($start);
-            }
+        if (!$start instanceof DateTime) {
+            $start = new DateTime(is_numeric($start) ? "@$start" : $start);
+        }
 
-            if (is_numeric($stop)) {
-                $stop = new DateTime("@$stop");
-            } else {
-                if (!$stop instanceof DateTime) {
-                    $stop = new DateTime($stop);
-                }
-            }
-        } catch (Exception $exception) {
-            throw new DruidException($exception->getMessage(), 0, $exception);
+        if (!$stop instanceof DateTime) {
+            $stop = new DateTime(is_numeric($stop) ? "@$stop" : $stop);
         }
 
         $this->intervals[] = new Interval($start, $stop);
