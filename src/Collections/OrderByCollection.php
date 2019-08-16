c02818aa516d103287f600ebc8d32a49172cc2c3
Teye Heimans <teye@level23.nl>
2019-08-16 10:30:32 +0200

    Added Collection Tests

--- src/Collections/OrderByCollection.php
+++ src/Collections/OrderByCollection.php
@@ -3,7 +3,6 @@ declare(strict_types=1);
 
 namespace Level23\Druid\Collections;
 
-use Level23\Druid\OrderBy\OrderBy;
 use Level23\Druid\OrderBy\OrderByInterface;
 
 class OrderByCollection extends BaseCollection
@@ -16,39 +15,6 @@ class OrderByCollection extends BaseCollection
     public function __construct(OrderByInterface ...$orderByColumns)
     {
         $this->items = $orderByColumns;
-    }
-
-    /**
-     * @param \Level23\Druid\OrderBy\OrderByInterface $orderBy
-     */
-    public function add(OrderByInterface $orderBy)
-    {
-        $this->items[] = $orderBy;
-    }
-
-    /**
-     * Make a dimension collection of the given array.
-     * For associative array's, we expect the key to be the "dimension" and the value the "output name".
-     *
-     * @param array $orderByColumns
-     *
-     * @return \Level23\Druid\Collections\OrderByCollection
-     */
-    public static function make(array $orderByColumns): OrderByCollection
-    {
-        $objects = [];
-
-        foreach ($orderByColumns as $key => $value) {
-            if ($value instanceof OrderBy) {
-                $objects[] = $value;
-            } elseif (is_numeric($key)) {
-                $objects[] = new OrderBy($value);
-            } else {
-                $objects[] = new OrderBy($key, $value);
-            }
-        }
-
-        return new OrderByCollection(...$objects);
     }
 
     /**
---
+++ tests/Collections/BaseCollectionTest.php
@@ -0,0 +1,118 @@
+<?php
+
+declare(strict_types=1);
+
+namespace tests\Level23\Druid\Collections;
+
+use Level23\Druid\Aggregations\SumAggregator;
+use Level23\Druid\Collections\AggregationCollection;
+use tests\TestCase;
+
+class BaseCollectionTest extends TestCase
+{
+    public function testGetType()
+    {
+        $collection = new AggregationCollection();
+        $iterator   = $collection->getIterator();
+
+        $this->assertInstanceOf(\ArrayIterator::class, $iterator);
+        $this->assertEquals([], $iterator->getArrayCopy());
+
+        $item = new SumAggregator('items');
+
+        $collection->add($item);
+
+        $iterator = $collection->getIterator();
+        $this->assertInstanceOf(\ArrayIterator::class, $iterator);
+
+        $this->assertEquals([$item], $iterator->getArrayCopy());
+    }
+
+    public function testAdd()
+    {
+        $collection = new AggregationCollection();
+
+        $item = new SumAggregator('items');
+        $collection->add($item);
+
+        $this->assertEquals($collection->offsetGet(0), $item);
+
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('We only accept instances of type');
+        $collection->add('hallo');
+    }
+
+    public function testOffsetExists()
+    {
+        $collection = new AggregationCollection();
+
+        $this->assertFalse($collection->offsetExists(0));
+
+        $item = new SumAggregator('items');
+        $collection->add($item);
+
+        $this->assertTrue($collection->offsetExists(0));
+    }
+
+    public function testOffsetGet()
+    {
+        $collection = new AggregationCollection();
+
+        $this->assertEquals(null, $collection->offsetGet(0));
+
+        $item = new SumAggregator('items');
+        $collection->add($item);
+
+        $this->assertEquals($item, $collection->offsetGet(0));
+    }
+
+    public function testOffsetSet()
+    {
+        $collection = new AggregationCollection();
+
+        $item = new SumAggregator('items');
+        $collection->offsetSet(12, $item);
+
+        $this->assertEquals($item, $collection->offsetGet(12));
+
+        $collection->offsetSet(null, $item);
+        $this->assertEquals($item, $collection->offsetGet(13));
+    }
+
+    public function testOffsetSetIncorrectType()
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('We only accept');
+
+        $collection = new AggregationCollection();
+        $collection->offsetSet(1, 'hallo');
+    }
+
+    public function testOffsetUnset()
+    {
+        $collection = new AggregationCollection();
+
+        $item = new SumAggregator('items');
+        $collection->offsetSet(12, $item);
+
+        $this->assertEquals($item, $collection->offsetGet(12));
+
+        $collection->offsetUnset(12);
+
+        $this->assertNull($collection->offsetGet(12));
+    }
+
+    public function testCount()
+    {
+        $collection = new AggregationCollection();
+
+        $this->assertEquals(0, $collection->count());
+
+        $item = new SumAggregator('items');
+        $collection->add($item);
+
+        $this->assertEquals(1, $collection->count());
+
+        $this->assertNull($collection->offsetGet(12));
+    }
+}
