54e3e7aa004a381cb2a43c8363769c41d9512894
Teye Heimans <teye@level23.nl>
2019-08-20 11:39:36 +0200

    Added virtual column support

---
+++ src/Concerns/HasVirtualColumns.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Level23\Druid\Concerns;
+
+use Level23\Druid\VirtualColumns\VirtualColumn;
+
+trait HasVirtualColumns
+{
+    /**
+     * @var array|\Level23\Druid\VirtualColumns\VirtualColumnInterface[]
+     */
+    protected $virtualColumns = [];
+
+    /**
+     * Create a virtual column.
+     *
+     * Virtual columns are queryable column "views" created from a set of columns during a query.
+     *
+     * A virtual column can potentially draw from multiple underlying columns, although a virtual column always
+     * presents itself as a single column.
+     *
+     * Virtual columns can be used as dimensions or as inputs to aggregators.
+     *
+     * @param string $as
+     * @param string $expression
+     * @param string $outputType
+     *
+     * @return $this
+     * @see https://druid.apache.org/docs/latest/misc/math-expr.html
+     */
+    public function virtualColumn(string $as, string $expression, $outputType = 'string')
+    {
+        $this->virtualColumns[] = new VirtualColumn($as, $expression, $outputType);
+
+        return $this;
+    }
+}
